d73fc2f67f19d711ea668466a5e538970a698aef
Júlio Vilela <julio.tabom@gmail.com>
2018-11-19 08:24:55 -0200

    API autenticação

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\API;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\Validator;
 use JWTAuth;
-use Validator;
-use DB;
+use Hash;
 
 
 
@@ -14,9 +16,28 @@ class AuthController extends Controller
 {
     public function register(Request $request){
        
-        #TODO
+        $validator = Validator::make($request->all(), [
+            'name'    => 'required|string',
+            'email'   => 'required|string|email|unique:users',
+            'password'=> 'required|confirmed'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $user = User::create([
+            'name'     => $request->get('name'),
+            'email'    => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+    
+        $token = JWTAuth::fromUser($user);
+        
+        return response()->json(compact('user','token'),201);
+        
     }
 
+   
+   
     public function login(Request $request){
 
         $credentials = $request->only('email', 'password');
@@ -24,9 +45,34 @@ class AuthController extends Controller
         $requer = ['email' => 'required|email', 'password' => 'required'];
 
         $validator = Validator::make($credentials,$requer );
-        
-        
-        
+
+        if($validator->fails()){
+            return response()->json([
+                
+                'status'  => 'erro',
+                'mensagem'=>  $validator->messages()
+            ]);
+        }
+
+        try{
+            if(!$token = JWTAuth::attempt($credentials)){
+                return response()->json([
+                    'status'  => 'erro',
+                    'mensagem'=> 'Conta não encontrada!'],401);
+            }
+
+        }catch(JWTException $e){
+            return response()->json([
+                'status'  => 'erro',
+                'mensagem'=> 'Erro, tente novamente!'],500);
+        }
+
+        return response()->json([
+            'status' => 'logado!',
+            'data'   => [
+                'token' => $token
+            ]]);
+
         /*
         $credentials = request(['email', 'password']);
 
@@ -41,6 +87,24 @@ class AuthController extends Controller
             'expires' =>auth('api')->factory()->getTTL() * 60]);
         */
     
+    }
+
+    public function logout(Request $request){
+
+        $token = $request->input(‘token’);
+
+        try {
+            JWTAuth::invalidate($token);
+            return response()->json([
+            'status'  => 'sucesso',
+            'message' => 'Deslogado']);
+        } catch (JWTException $e) {
+
+            return response()->json([
+                'status'   => 'erro',
+                'mesangem' => 'Não deslogado!'],500);
+        }
+
     }
     
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,8 @@ use Illuminate\Http\Request;
 
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
+
+Route::group(['middleware' =>'auth.jwt'], function(){
+    Route::get('logout','AuthController@logout');
+    Route::get('user','AuthController@user');
+});
